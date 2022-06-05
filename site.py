adfc107d1cf0d158bc0787db12831fdaeb9e2bf7
Dan Mayer <dan@mayer.cool>
2022-06-04 22:12:43 -0700

    update todo

--- site.py
+++ site.py
@@ -6,7 +6,8 @@ This is my first attempt at a PEP 8-compilant python script that I can be
 proud of. It is and will always be designed for personal use but can easily be
 borrowed by replacing the contents of the 'source' folder.
 
-TODO: gitpython integration
+TODO: gitpython integration for tracking which pages need to be (re)generated
+TODO: gitpython integration for pushing to github pages
 TODO: do images work?
 TODO: some sort of header and footer? or just templating?
 TODO: autogenerating links to internal pages
